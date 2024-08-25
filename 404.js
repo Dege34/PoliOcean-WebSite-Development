79790a815a1a7c85978fe9553387203664626b2d
Dogan Ege BULTE <112330742+Dege34@users.noreply.github.com>
2024-08-25 16:34:09 +0200

    PoliTOcean Web
    I worked on the website development part of Poltoceean for about 2 months as a Project manager. I would like to thank to our design manager and department head Matteo Sala. Consequently, we created a website, where I experienced some new things and improved myself in those areas.
    
    ALL LEGAL RIGHTS RESERVED Copyright © 2024 Dogan Ege BULTE <dege.bulte@studenti.polito.it>

---
+++ 404.js
@@ -0,0 +1,8 @@
+/****************************************************
+* Typing Text script- By Twey @ Dynamic Drive Forums
+* Visit Dynamic Drive for this script and more: http://www.dynamicdrive.com
+* This notice MUST stay intact for legal use
+****************************************************/
+new TypingText(document.getElementById("body"));
+
+  
