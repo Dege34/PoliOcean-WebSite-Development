7d13298eab7d879c08a5a040fde29b0df3249612
Dogan Ege BULTE <112330742+Dege34@users.noreply.github.com>
2024-08-25 17:49:33 +0200

    Delete 404.js

--- 404.js
+++
@@ -1,8 +0,0 @@
-/****************************************************
-* Typing Text script- By Twey @ Dynamic Drive Forums
-* Visit Dynamic Drive for this script and more: http://www.dynamicdrive.com
-* This notice MUST stay intact for legal use
-****************************************************/
-new TypingText(document.getElementById("body"));
-
-  
